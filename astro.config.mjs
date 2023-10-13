162ebf7c690097daed5aeabe06ac76a34f76aff5
simon <simonn@duck.com>
2023-10-12 22:19:06 -0400

    clouds

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import mdx from "@astrojs/mdx";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +12,7 @@ export default defineConfig({
   integrations: [preact(), tailwind(), sitemap(), mdx()],
   legacy: {
     astroFlavoredMarkdown: true
-  }
+  },
+  output: "server",
+  adapter: cloudflare()
 });
